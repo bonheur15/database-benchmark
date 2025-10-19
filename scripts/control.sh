ed923ef8c2f1112199286042066b4ba6d380bb5e
bonheur15 <rumanzibonheur@gmail.com>
2025-10-19 17:00:59 +0200

    Revert back from sudo to no-sudo

--- scripts/control.sh
+++ scripts/control.sh
@@ -4,10 +4,10 @@ set -e
 
 case "$1" in
   up)
-    sudo docker compose up -d
+    docker compose up -d
     ;;
   down)
-    sudo docker compose down
+    docker compose down
     ;;
   *)
     echo "Usage: $0 {up|down}"
